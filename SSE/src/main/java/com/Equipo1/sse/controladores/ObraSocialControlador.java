a7ad908f90b471dbe410ee53eefd5003052988bd
Rlalexisrl <lantmannalexis@gmail.com>
2024-02-15 19:03:44 -0300

    agrego controlador de turno e historial

--- SSE/src/main/java/com/Equipo1/sse/controladores/ObraSocialControlador.java
+++ SSE/src/main/java/com/Equipo1/sse/controladores/ObraSocialControlador.java
@@ -13,11 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author ALEXIS.R.L
--- SSE/src/main/java/com/Equipo1/sse/controladores/PacienteControlador.java
+++ SSE/src/main/java/com/Equipo1/sse/controladores/PacienteControlador.java
@@ -5,15 +5,7 @@
  */
 package com.Equipo1.sse.controladores;
 
-import com.Equipo1.sse.entidades.Paciente;
-import com.Equipo1.sse.entidades.Turno;
-import com.Equipo1.sse.entidades.Usuario;
-import com.Equipo1.sse.servicios.TurnoServicio;
-import java.util.List;
-import javax.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,26 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/paciente")
-public class PacienteControlador
-{
-	@Autowired
-	private TurnoServicio turnoServicio;
-	
-	@GetMapping("/")
-	public String paciente()
-	{
-		return "paciente.html";
-	}
-	
-	@GetMapping("/turnos")
-	public String turnos(ModelMap modelo, HttpSession session)
-	{
-		Usuario usuario = (Usuario) session.getAttribute("usuarioSession");
-		if(usuario instanceof Paciente)
-		{
-			List<Turno> turnos = turnoServicio.buscarPorNumeroAfiliado(((Paciente)usuario).getNumAfiliado());
-			modelo.put("turnos", turnos);
-		}
-		return "turnos.hmtl";
-	}
+public class PacienteControlador {
+
+    @GetMapping("/")
+    public String paciente() {
+        return "paciente.html";
+    }
 }
