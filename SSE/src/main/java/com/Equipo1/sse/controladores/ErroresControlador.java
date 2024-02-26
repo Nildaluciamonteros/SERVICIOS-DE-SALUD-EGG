ec2c5dd8e1c52e8c3e5b26e14a4d21e1ac4ff868
Nilda Lucia Monteros <nildaluciamonteros@gmail.com>
2024-02-26 20:32:39 -0300

    espero no cagarla

--- SSE/src/main/java/com/Equipo1/sse/controladores/ErroresControlador.java
+++ SSE/src/main/java/com/Equipo1/sse/controladores/ErroresControlador.java
@@ -17,53 +17,49 @@ import org.springframework.web.servlet.ModelAndView;
  * @author Nico
  */
 @Controller
-public class ErroresControlador implements ErrorController
-{
-	@RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView renderErrorPage(HttpServletRequest httpRequest)
-	{
-		ModelAndView errorPage = new ModelAndView("error");
-		
-		String errorMsg = "";
-		
-		int httpErrorCode = getErrorCode(httpRequest);
-		
-		switch(httpErrorCode)
-		{
-			case 400: {
-				errorMsg = "El recurso solicitado no existe.";
-				break;
-			}
-			case 403: {
-				errorMsg = "No tiene permisos para acceder al recurso.";
-				break;
-			}
-			case 401: {
-				errorMsg = "No se encunetra autorizado.";
-				break;
-			}
-			case 404: {
-				errorMsg = "El recurso solicitado no fue encontrado.";
-				break;
-			}
-			case 500: {
-				errorMsg = "Ocurrió un error interno.";
-				break;
-			}
-		}
-		errorPage.addObject("codigo",httpErrorCode);
-		errorPage.addObject("mensaje",errorMsg);
-		
-		return errorPage;
-	}
-	
-	public int getErrorCode(HttpServletRequest httpRequest)
-	{
-		return (Integer)httpRequest.getAttribute("javax.servlet.error.status_code");
-	}
-	
-	public String getErrorPath()
-	{
-		return "/error.html";
-	}
+public class ErroresControlador implements ErrorController {
+
+    @RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+        ModelAndView errorPage = new ModelAndView("error");
+
+        String errorMsg = "";
+
+        int httpErrorCode = getErrorCode(httpRequest);
+
+        switch (httpErrorCode) {
+            case 400: {
+                errorMsg = "El recurso solicitado no existe.";
+                break;
+            }
+            case 403: {
+                errorMsg = "No tiene permisos para acceder al recurso.";
+                break;
+            }
+            case 401: {
+                errorMsg = "No se encunetra autorizado.";
+                break;
+            }
+            case 404: {
+                errorMsg = "El recurso solicitado no fue encontrado.";
+                break;
+            }
+            case 500: {
+                errorMsg = "Ocurrió un error interno.";
+                break;
+            }
+        }
+        errorPage.addObject("codigo", httpErrorCode);
+        errorPage.addObject("mensaje", errorMsg);
+
+        return errorPage;
+    }
+
+    public int getErrorCode(HttpServletRequest httpRequest) {
+        return (Integer) httpRequest.getAttribute("javax.servlet.error.status_code");
+    }
+
+    public String getErrorPath() {
+        return "/error.html";
+    }
 }
